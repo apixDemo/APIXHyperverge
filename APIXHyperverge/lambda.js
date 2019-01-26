62de6d60879a94105209da697281071d4b12d850
apixDemo <afin.apix.demo@gmail.com>
2019-01-26 16:33:51 +0800

    test1

--- APIXHyperverge/lambda.js
+++ APIXHyperverge/lambda.js
@@ -2,7 +2,7 @@
 let Swagger = require('swagger-client');
 
 exports.handler = function (event, context, callback) {
-
+    console.log(event);
     Swagger.http({
         url: 'https://services.apixplatform.com/api-sandbox/application/token',
         method: 'post',
@@ -14,6 +14,7 @@ exports.handler = function (event, context, callback) {
         })
     }).then((response) => {
         var access_token = "bearer " + response.body.access_token;
+        console.log("Access token received");
         // Insert new API calls here to call with APIX Access Token
         Swagger.http({
             url: `https://api.apixplatform.com/facematch/1.0/v1/photo/verifyPair`,
@@ -22,13 +23,30 @@ exports.handler = function (event, context, callback) {
             headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f", "X-Authorization": access_token, "Accept": "application/json", "Content-Type": "application/x-www-form-urlencoded" },
             body: `image1=` + event.image1 + `&image2=` + event.image2 + `&type=id`
         }).then((response) => {
+            response = {
+                "isBase64Encoded": true,
+                "statusCode": 200,
+                "headers": {},
+                "body": response
+            }
             callback(null, response);
         }).catch((err) => {
-            callback(err);
+            response = {
+                "isBase64Encoded": true,
+                "statusCode": err.statusCode,
+                "headers": {},
+                "body": err//.response.statusText
+            }
+            callback(null,response);
         });
     }).catch((err) => {
-        console.log(err);
-        callback("Execution failed");
+        response = {
+                "isBase64Encoded": true,
+                "statusCode": err.statusCode,
+                "headers": {},
+                "body": err.response.statusText
+            }
+        callback(null,response);
     });
 
 }
